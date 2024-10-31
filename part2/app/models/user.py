899d6ce636fb600d80e84df0ccb52c15c9093439
kimikoultramega <9379@holbertonstudents.com>
2024-10-31 15:13:03 -0300

    adding the model of user class

---
+++ part2/app/models/user.py
@@ -0,0 +1,19 @@
+from .import BaseModel
+import re
+
+class User(BaseModel):
+    def __init__(self, first_name, last_name, email, is_admin=False):
+        super().__init__() # Llama al constructor de la super clase
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
+        self.validate() # Valida los atrivutos
+
+    def validate(self):
+        if len(self.first_name) > 50:
+            raise ValueError("First name cannot exceed 50 characters.")
+        if len(self.last_name) > 50:
+            raise ValueError("Last name cannot exceed 50 characters.")
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", self.email):
+            raise ValueError("Invalid email format.")
