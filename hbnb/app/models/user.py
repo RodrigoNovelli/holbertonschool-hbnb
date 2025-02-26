30b21aec8d9d6e2f2d0583df83f83b7c58da74bd
Rodrigo Novelli <Rodrigonoelli21@hotmail.com>
2025-02-26 15:14:52 -0300

    Solvingrecursion problem with users model

--- hbnb/app/models/user.py
+++ hbnb/app/models/user.py
@@ -1,60 +1,57 @@
 from . import BaseModel
 import re
 
+
 class User(BaseModel):
-    def __init__(self, first_name, last_name, email, is_admin= False):
+    def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
-    
+
     @property
     def first_name(self):
-        return self.first_name
-    
+        return self._first_name
+
     @first_name.setter
     def first_name(self, string):
         if len(string) <= 50 and isinstance(string, str):
-            self.first_name = string
+            self._first_name = string
         else:
-            raise ValueError('First name must be a string and be under 50 char')    
+            raise ValueError("""
+                             First name must be a string and be under 50 char
+                             """)
+
     @property
     def last_name(self):
-        return self.last_name
-    
+        return self._last_name
+
     @last_name.setter
     def last_name(self, string):
-        if len(string) <= 50 and isintance(string, str):
-            self.last_name = string
+        if len(string) <= 50 and isinstance(string, str):
+            self._last_name = string
         else:
             raise ValueError('Last name must be a string and be under 50 char')
-    
+
     @property
     def email(self):
-        return self.email
-    
+        return self._email
+
+    def validate_email(self, email):
+        validator = r'^[a-zA-z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        if re.match(validator, email):
+            return True
+        else:
+            return False
+
     @email.setter
     def email(self, string):
-        if validate_email(string) == True:
-            self.email = string
+        if self.validate_email(string) is True:
+            self._email = string
         else:
             raise ValueError('Invalid email')
-        
-    
-    def validate_email(self, email):
-                validator = r'^[a-zA-z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-                if re.match(validator, email):
-                    return email
-                else:
-                    raise ValueError("Invalid email")
-    
+
     def add_places(self, places):
         self.places.append(places)
-        places.owner = self
-    
-    
-    
-    
-
-        
+        places._owner = self
