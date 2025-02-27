be4d864f65aae7390180748a5f11299500249a57
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-02-27 15:54:47 -0300

    Adding amenities endpoint

---
+++ hbnb/app/api/v1/amenities.py
@@ -0,0 +1,41 @@
+from flask_restx import Namespace, Resource, fields
+from app.services import facade
+
+api = Namespace('amenities', description='Amenity operations')
+
+# Define the amenity model for input validation and documentation
+amenity_model = api.model('Amenity', {
+    'name': fields.String(required=True, description='Name of the amenity')
+})
+
+@api.route('/')
+class AmenityList(Resource):
+    @api.expect(amenity_model)
+    @api.response(201, 'Amenity successfully created')
+    @api.response(400, 'Invalid input data')
+    def post(self):
+        amenity_data = api.payload
+        new_amenity = facade.create_amenity(amenity_data)
+        return {'id': new_amenity.id, 'name': new_amenity.name}
+
+    @api.response(200, 'List of amenities retrieved successfully')
+    def get(self):
+        return facade.get_all_amenities.__dict__
+
+@api.route('/<amenity_id>')
+class AmenityResource(Resource):
+    @api.response(200, 'Amenity details retrieved successfully')
+    @api.response(404, 'Amenity not found')
+    def get(self, amenity_id):
+        amenity_by_id = facade.get_amenity(amenity_id)
+        return {'id': amenity_by_id.id, 'name': amenity_by_id.name}
+
+    @api.expect(amenity_model)
+    @api.response(200, 'Amenity updated successfully')
+    @api.response(404, 'Amenity not found')
+    @api.response(400, 'Invalid input data')
+    def put(self, amenity_id):
+        amenity_data = api.payload
+        facade.update_amenity(amenity_id, amenity_data)
+        updated_amenity = facade.get_amenity(amenity_id)
+        return {'id': updated_amenity.id, 'name': updated_amenity.name}
--- hbnb/app/services/facade.py
+++ hbnb/app/services/facade.py
@@ -1,6 +1,7 @@
 from app.persistence.repository import InMemoryRepository
 from app.api.v1 import users
 from app.models.user import User
+from app.models.amenity import Amenity 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
@@ -18,3 +19,18 @@ class HBnBFacade:
 
     def get_user_by_email(self, email):
         return self.user_repo.get_by_attribute('email', email)
+    
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        updated_a = self.amenity_repo.update(amenity_id, amenity_data)
+        return updated_a
