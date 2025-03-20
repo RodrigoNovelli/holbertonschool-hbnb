92fed84681def506c12cd2718158fef01aa93859
balemansteve <balemansteve@gmail.com>
2025-03-20 17:16:29 -0300

    Added config Class import in app

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -7,15 +7,16 @@ from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from config import Config, DevelopmentConfig
 
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
 
-def create_app(config_class="config.DevelopmentConfig"):
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+    api = Api(app, version='2.0', title='HBnB API', description='HBnB Application API')
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
@@ -23,6 +24,8 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(auth_ns, path='/api/v1/login')
+
+    # Initialize extensions with the app
     bcrypt.init_app(app)
     jwt.init_app(app)
     return app
