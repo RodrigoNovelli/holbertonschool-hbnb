cd5a404fb924feb5136b17c80e87f7608ee52230
kevinacostaa <kevinmag999@gmail.com>
2025-04-02 17:15:44 -0300

    review

--- part3/hbnb/app/models/review.py
+++ part3/hbnb/app/models/review.py
@@ -2,13 +2,20 @@ from app.models.base import BaseModel
 from app.models.user import User
 from app.models.place import Place
 from app import db
+from sqlalchemy.orm import validates, relationship
+from sqlalchemy import Table, Column, Integer, ForeignKey
 
 
-class Review(BaseModel, db.Model):
+class Review(BaseModel):
     __tablename__ = 'review'
 
     text = db.Column(db.String(125), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    place_id = Column(Integer, ForeignKey('place.id'), nullable=False)
+    user = relationship('User', back_populates='reviews')
+    place = relationship('Place', back_populates='reviews')
+
     def __init__(self, text, rating, place_id, user_id):
         super().__init__()
         self.text = text
@@ -16,25 +23,19 @@ class Review(BaseModel, db.Model):
         self.place_id = place_id
         self.user_id = user_id
     
-    @property
-    def text(self):
-        return self._text
     
-    @text.setter
-    def text(self, string):
+    @validates("text")
+    def validate_text(self, key, string):
         if isinstance(string, str):
-            self._text = string
+            return string
         else:
             raise ValueError("Text must be a string")
     
-    @property
-    def rating(self):
-        return self._rating
     
-    @rating.setter
-    def rating(self, num):
+    @validates("rating")
+    def validate_rating(self, key, num):
         if num <= 5 and num >= 1 and isinstance(num, int):
-            self._rating = num
+            return num
         else:
             raise ValueError('Rating mus be a number between 1 and 5')
     
