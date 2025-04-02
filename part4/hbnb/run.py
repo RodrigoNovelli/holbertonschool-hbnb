5f48082f27c214f7852964bc393d95afbce82e85
kevinacostaa <kevinmag999@gmail.com>
2025-04-02 10:12:57 -0300

    database

--- part4/hbnb/run.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from app.services import facade
-from app.models.amenity import Amenity
-from app.models.user import User
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
