336df21c6be006e8bdf1276bef70eac02f1efdbd
balemansteve <balemansteve@gmail.com>
2025-04-02 11:12:51 -0300

    Add '()' in 'class HBnBFacade'

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -9,11 +9,11 @@ from app.services.repositories.review_repository import ReviewRepository
 from app.services.repositories.amenity_repository import AmenityRepository
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = UserRepository
-        self.place_repo = PlaceRepository
-        self.review_repo = ReviewRepository
-        self.amenity_repo = AmenityRepository
-    
+        self.user_repo = UserRepository()
+        self.place_repo = PlaceRepository()
+        self.review_repo = ReviewRepository()
+        self.amenity_repo = AmenityRepository()
+
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
