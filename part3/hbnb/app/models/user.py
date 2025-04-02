2b0f60bddeb1df0d07c8e30a54d94282f27f424d
balemansteve <balemansteve@gmail.com>
2025-04-02 11:11:07 -0300

    Implement 'password setter' and 'hash_password'

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -5,20 +5,20 @@ import re
 
 class User(BaseModel, db.Model):
     __tablename__ = 'users'
-    
+
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
-    password = db.Column(db.String(128), nullable=False)
+    _password_hash = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
-    
+
     def __init__(self, first_name, last_name, email, password, is_admin=False):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
-        self.hash_password(password)
+        self.password = password
 
     @property
     def first_name(self):
@@ -65,17 +65,18 @@ class User(BaseModel, db.Model):
     def add_places(self, places):
         self.places.append(places)
         places._owner = self
-    
+
     @property
     def password(self):
         raise AttributeError('Password is not readable')
 
-    def hash_password(self, password):
+    @password.setter
+    def password(self, password):
         """Hashes the password before storing it."""
         from app import bcrypt
-        self._password = bcrypt.generate_password_hash(password).decode('utf-8')
-    
+        self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
     def verify_password(self, password):
         """Verifies if the provided password matches the hashed password."""
         from app import bcrypt
-        return bcrypt.check_password_hash(self._password, password)
+        return bcrypt.check_password_hash(self._password_hash, password)
