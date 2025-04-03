7e510b07f4e9385c25a1f7f15cbd76559c76b833
balemansteve <balemansteve@gmail.com>
2025-04-02 23:57:33 -0300

    Add users API

--- part3/hbnb/app/api/v1/users.py
+++ part3/hbnb/app/api/v1/users.py
@@ -1,7 +1,11 @@
 from flask_restx import Namespace, Resource, fields
-from app.models.user import User
 from app.services import facade
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask import request
+from flask_bcrypt import Bcrypt
+
+
+bcrypt = Bcrypt
 
 api = Namespace('users', description='User operations')
 
@@ -28,21 +32,17 @@ class AdminUserCreate(Resource):
             return {'error': 'Admin privileges required'}, 403
 
         user_data = api.payload
-        current_user = get_jwt_identity()
         email = user_data.get('email')
 
-        if current_user.get('is_admin'):
-            if facade.get_user_by_email(email):
-                return {'error': 'Email already registered'}, 400
-            
-            try:
-                new_user = facade.create_user(user_data)
-                return {'id': new_user.id, 'messagge': 'User succesfully created'}, 201
-            except ValueError as e:
-                return {'error': str(e)}, 400
-                
-        return {'error': 'Admin privileges required'}, 403
-    
+        if facade.get_user_by_email(email):
+            return {'error': 'Email already registered'}, 400
+
+        try:
+            new_user = facade.create_user(user_data)
+            return {'id': new_user.id, 'message': 'User successfully created'}, 201
+        except ValueError as e:
+            return {'error': str(e)}, 400
+
 @api.route('/')
 class UserList(Resource):
     @api.expect(user_model, validate=True)
@@ -53,14 +53,19 @@ class UserList(Resource):
         """Register a new user"""
         user_data = api.payload
         print('user data is {}'.format(user_data))
-        # Simulate email uniqueness check (to be replaced by real validation with persistence)
-        
-        existing_user = facade.get_user_by_email(email= user_data['email'])
+
+        existing_user = facade.get_user_by_email(email=user_data['email'])
         if existing_user:
             return {'error': 'Email already registered'}, 400
+
         new_user = facade.create_user(user_data)
-        return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email, 'password': new_user.password}, 201
-    
+        return {
+            'id': new_user.id, 
+            'first_name': new_user.first_name, 
+            'last_name': new_user.last_name, 
+            'email': new_user.email
+        }, 201
+
 @api.route('/<user_id>')
 @api.response(201, 'User', user_response_model)
 class UserResource(Resource):
@@ -78,7 +83,7 @@ class UserResource(Resource):
     @api.response(200, 'User updated successfully', user_model)
     @api.response(404, 'User not found')
     @api.response(400, 'Invalid input data')
-    def put (self, user_id, user_data):
+    def put (self, user_id):
         user_data = api.payload
     
         user = facade.get_user(user_id)
