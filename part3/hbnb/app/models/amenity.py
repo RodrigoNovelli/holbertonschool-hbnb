9bf53ac776c90b29ea6064d7d8799ccf23e8d76e
balemansteve <balemansteve@gmail.com>
2025-04-03 00:19:13 -0300

    Add amenity Models

--- part3/hbnb/app/models/amenity.py
+++ part3/hbnb/app/models/amenity.py
@@ -1,6 +1,5 @@
 from app.models.base import BaseModel
 from app import db
-from sqlalchemy.orm import validates
 
 class Amenity(BaseModel, db.Model):
     __tablename__ = 'amenity'
@@ -8,10 +7,14 @@ class Amenity(BaseModel, db.Model):
     def __init__(self, name):
         super().__init__()
         self.name = name
-
-    @validates("name")
-    def validates_name(self, key, string):
-        if len(string) <= 50:
-            self.name = string
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, string):
+        if len(string) <= 50 and isinstance(string, str):
+            self._name = string
         else:
-            raise ValueError('Name is too long')
+            raise ValueError('Name must be a string between 0 and 50 char')
